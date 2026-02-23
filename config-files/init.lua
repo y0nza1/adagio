d33cbe324fc7a4d6ac38e3e89ec2dcef4dd67b49
jen <jen@adagio>
2026-02-22 18:35:33 -0800

    Updated config files.

--- config-files/init.lua
+++ config-files/init.lua
@@ -1,9 +1,18 @@
-vim.opt.tabstop = 2         -- Number of spaces a <Tab> in the file counts for
-vim.opt.shiftwidth = 2      -- Number of spaces to use for each step of (auto)indent
-vim.opt.expandtab = true    -- Expand tabs to spaces when the <Tab> key is pressed
-vim.opt.softtabstop = 2     -- Number of spaces a <Tab> counts for in insert mode
+-- Line number visibility
+vim.opt.number = true 
+-- Remove squiggly fillchars
+vim.opt.fillchars = {eob = " "}
 
+-- Number of spaces a <Tab> in the file counts for
+vim.opt.tabstop = 2         
+-- Number of spaces to use for each step of (auto)indent
+vim.opt.shiftwidth = 2     
+-- Expand tabs to spaces when the <Tab> key is pressed
+vim.opt.expandtab = true    
+-- Number of spaces a <Tab> counts for in insert mode
+vim.opt.softtabstop = 2
 
+-- Makes NeoVim transparent
 function Transparent(color)
   -- color = color or "name_of_theme"
   vim.cmd.colorscheme(color)
